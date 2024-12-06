2374fc15ae46a97ffb68156a7442d86f5096a4b2
timvanharen <timvanharen@live.com>
2024-12-06 16:16:58 +0100

    Formulation of basis persuit denoising (rewriting convex -cardinality problem)
    And adding CVX solver to get an estimate

---
+++ Compressed_sensing.m
@@ -0,0 +1,36 @@
+clear all
+close all
+load("cs.mat")
+
+gamma = 0.1
+% Show the sparse data and data mask
+figure;
+plot(sampling_mask);
+title("Sampling mask");
+
+figure
+plot(x);
+title("true data vector")
+
+figure
+plot(real(F_us*x))
+title("Sampling_mask*x")
+
+figure;
+plot(real(X_us));
+title("X_us");
+
+cvx_begin
+    variable x_est(n)
+    minimize( norm(F_us*x_est-X_us, 2) + gamma*norm(x_est,1) )
+cvx_end
+
+plot(x_est)
+title("x estimated using CVX")
+disp(norm(x_est-x,2))
+
+% Determine sparsity
+% non_zero_array = find([X_us(1:end-1)]);
+% K = size(non_zero_array)
+% 
+% Z = [diff([1,non_zero_array(:).'+1])-1;X_us(non_zero_array(:).',1).'].'
